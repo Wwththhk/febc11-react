1532fcf3d6b6e1923f44aa31f2833e3210156de7
khk99 <khk990815@gmail.com>
2024-11-19 11:35:07 +0900

    🎉

--- workspace/ch01-buildup/counter/03/yong.js
+++ workspace/ch01-buildup/counter/03/yong.js
@@ -7,22 +7,22 @@ const yong = {
     const elem = document.createElement(tag);
 
     // 속성 추가
-    if (props) {
-      for (const attrName in props) {
+    if(props){
+      for(const attrName in props){
         elem.setAttribute(attrName, props[attrName]);
       }
     }
 
     // 자식 노드 추가
-    for (let child of children) {
-      if (typeof child === "string" || typeof child === "number") {
+    for(let child of children){
+      if(typeof child === 'string' || typeof child === 'number'){
         child = document.createTextNode(child);
       }
       elem.appendChild(child);
     }
 
     return elem;
-  },
+  }
 };
 
 export default yong;
--- workspace/ch02-start/vite/src/App.jsx
+++ workspace/ch02-start/vite/src/App.jsx
@@ -1,25 +1,8 @@
-import { useState } from "react";
-import Title from "./components/Title";
+import Todo from "@pages/todo/Todo"
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Title />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+      <Todo />
+  )
 }
-
-export default App;
+export default App
