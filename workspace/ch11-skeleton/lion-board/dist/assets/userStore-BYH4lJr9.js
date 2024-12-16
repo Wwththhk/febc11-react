2cc857b4bc577c685488fc78e80fb05131c75ec1
khk99 <khk990815@gmail.com>
2024-12-16 09:30:04 +0900

    1

---
+++ workspace/ch11-skeleton/lion-board/dist/assets/userStore-BYH4lJr9.js
@@ -0,0 +1,1 @@
+import{w as r,x as t,y as o}from"./index-B8grBGTx.js";const a=e=>({user:null,setUser:s=>e({user:s}),resetUser:()=>e({user:null})}),n=r(t(a,{name:"user",storage:o(()=>sessionStorage)}));export{n as u};
